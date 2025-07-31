0b12d1306c82d306af3e16ebf36f6eaf20649e14
obm <mbaris@metu.edu.tr>
2025-07-31 16:51:40 +0300

    Initial commit

---
+++ source/conf.py
@@ -0,0 +1,48 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Q-CADE'
+copyright = '2025, O. Baris Malcioglu'
+author = 'O. Baris Malcioglu'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+        'myst_parser',
+        ]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+
+# Furo
+html_title = "Q-CADE Lab"
+html_theme_options = {
+    "light_logo": "Q-CADE_logo-light.png",
+    "dark_logo": "Q-CADE_logo-dark.png",
+}
+
+# Myst
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+]
